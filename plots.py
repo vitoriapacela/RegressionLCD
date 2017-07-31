50c3298865f87787528464ee5a39b79a1757158e
vitoriapacela <vitoriapacela@gmail.com>
2017-07-31 02:43:36 -0700

    updating plots

--- plots.py
+++ plots.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
 from scipy.stats import norm
+import os, sys
 
 def show_losses(histories):
     '''
@@ -105,7 +106,8 @@ def plotPredictedXTarget(target, predicted, lim_l=0, lim_r=550):
     plt.figure(figsize=(5, 5))
     plt.xlabel("Target energy (GeV)")
     plt.ylabel("Predicted energy (GeV)")
-    plt.title("Predicted energy against the true energy of the particle, energies between %d and %d GeV" % (lim_l, lim_r))
+    plt.title("Predicted X true energy \n Energies between %d and %d GeV" % (lim_l, lim_r))
+    #plt.title("Predicted X true energy")
 
     plt.scatter(target, predicted, color='g', alpha=0.5)
 
@@ -115,9 +117,10 @@ def plotPredictedXTarget(target, predicted, lim_l=0, lim_r=550):
     plt.legend()
     plt.show()
 
-def histEDif(difference, nbins=50, lim=25, lim_l=0, lim_r=550):
+    
+def histEDif(target, pred, nbins=50, lim=25, lim_l=0, lim_r=550):
     '''
-    This function plots histogram of the difference between the predicted energy and the target energy (GeV).
+    This function plots histogram of the difference between the target energy and the predicted energy (GeV).
     :param difference: energy difference as numpy.ndarray. Suggestion: use dif() function here.
     :param nbins: number of bins desired for the histogram, as integer.
     :param lim: (int, float) defines the minimum and maximum values for the x axis.
@@ -125,60 +128,75 @@ def histEDif(difference, nbins=50, lim=25, lim_l=0, lim_r=550):
     :param lim_r: (int, float) defines the maximum energy value, used in the title of the plot.
     '''
 
+    difference = dif(target, pred)
+    
     # the histogram of the data
-    n, bins, patches = plt.hist(difference, nbins, normed=1, facecolor='green', alpha=0.75)
-
-    # best fit of data
-    (mu, sigma) = norm.fit(difference)
-
-    # add a 'best fit' line
-    y = mlab.normpdf(bins, mu, sigma)
-    l = plt.plot(bins, y, 'r--', linewidth=1)
-
-    plt.xlabel('Difference between the predicted energy and the target energy (GeV)')
-    plt.ylabel('Probability')
-    plt.title("Energy difference in the regression model, energies between %d and %d GeV" % (lim_l, lim_r))
-
-    plt.xlim(-lim, lim)
-
-    plt.show()
-    #plt.savefig("histDif_%d_%d.jpg" % (lim_l, lim_r))
-
-
-def histRelDif(difference, nbins=50, lim=0.2, lim_l=0, lim_r=550):
-    '''
-    Plots histogram of the normalized energy difference (%).
-    :param difference: energy difference as numpy.ndarray. Suggestion: use dif() function here.
-    :param nbins: number of bins desired for the histogram, as integer.
-    :param lim: (int, float) defines the minimum and maximum values for the x axis.
-    :param lim_l: (int, float) defines the minimum energy value, used in the title of the plot.
-    :param lim_r: (int, float) defines the maximum energy value, used in the title of the plot.
-    '''
-
-    # the histogram of the data
-    n, bins, patches = plt.hist(difference, nbins, normed=1, facecolor='green', alpha=0.75)
-
+    
     mean = np.mean(difference)
     print mean
 
     std = np.std(difference)  # standard deviation
     print std
+    
+    error=std/np.sqrt(len(target))
+    #labels=['Mean: %.2f' % mean, 'Standard deviation: %.2f' % std]
+    
+    n, bins, patches = plt.hist(difference, nbins, normed=1, facecolor='green', alpha=0.75)
+    
+    plt.text(3, 0.12, 'Mean: %.2f $\pm$ %.2f \nStd. dev.: %.2f' % (mean, error, std))
 
-    # best fit of data
-    (mu, sigma) = norm.fit(difference
-                           # , range = (-std,std)
-                           )
-    print mu, sigma
-
-    # add a "best fit" line
-    y = mlab.normpdf(bins, mu, sigma)  # normal probability density function
-    l = plt.plot(bins, y, 'r--', linewidth=1)
+    #plt.xlabel('Difference between true and predicted energy (GeV)')
+    plt.xlabel(r'$E_{true} - E_{pred}$ (GeV)', size=16)
+    plt.ylabel('Probability', size=16)
+    plt.title("Energy difference \n Energies between %d and %d GeV" % (lim_l, lim_r), size=16)
+    #plt.title("Energy difference")
 
     plt.xlim(-lim, lim)
-    plt.xlabel('Relative difference between the predicted energy and the target energy (%)')
-    plt.ylabel('Probability')
-    plt.title("Relative energy difference in the regression model, energies between %d and %d GeV" % (lim_l, lim_r))
 
+    plt.legend(loc='best')
+    
+    plt.show()
+    #plt.savefig("histDif_%d_%d.jpg" % (lim_l, lim_r))
+    
+
+def histRelDif(target, pred, nbins=50, lim=0.2, lim_l=0, lim_r=550):
+    '''
+    Plots histogram of the normalized energy difference (%).
+    :param difference: energy difference as numpy.ndarray. Suggestion: use rDif() function here.
+    :param nbins: number of bins desired for the histogram, as integer.
+    :param lim: (int, float) defines the minimum and maximum values for the x axis.
+    :param lim_l: (int, float) defines the minimum energy value, used in the title of the plot.
+    :param lim_r: (int, float) defines the maximum energy value, used in the title of the plot.
+    '''
+
+    difference = rDif(target, pred)
+    
+    # the histogram of the data
+    
+    mean = np.mean(difference)
+    print mean
+
+    std = np.std(difference)  # standard deviation
+    print std
+    
+    #labels=['Mean: %.2f' % mean, 'Standard deviation: %.2f' % std]
+    error=std/np.sqrt(len(target))
+    print error
+    
+    n, bins, patches = plt.hist(difference, nbins, normed=1, facecolor='green', alpha=0.75)
+
+    plt.text(0.015, 20, 'Mean: %.5f $\pm$ %.5f \nStd. dev.: %.2f' % (mean, error, std))
+    #plt.text(3, 0.12, 'Mean: %.2f $\pm$ %.2f \nStd. dev.: %.2f' % (mean, error, std))
+
+
+    plt.xlim(-lim, lim)
+    #plt.xlabel('Relative difference between true and predicted energy (%)')
+    plt.xlabel(r'$\frac{(E_{true} - E_{pred})}{E_{true}}$ (GeV)', size=18)
+    plt.ylabel('Probability', size=16)
+    plt.title("Relative energy difference \n Energies between %d and %d GeV" % (lim_l, lim_r), size=16)
+    #plt.title("Relative energy difference")
+    
+    plt.legend(loc='best')
     plt.show()
     # plt.savefig("histRDif_%d_%d.jpg" % (lim_l, lim_r))
 
@@ -195,7 +213,8 @@ def plotRelXTarget(target, relative, lim_l=0, lim_r=500):
     plt.figure(figsize=(5, 5))
     plt.xlabel("Target energy (GeV)")
     plt.ylabel("Relative energy difference (%)")
-    plt.title("Testing set: Comparing the relative energy difference with the target energy of the particle")
+    #plt.title("Relative energy difference X Target energy")
+    plt.title("Relative energy difference X Target energy \n Energies between %d and %d GeV" % (lim_l, lim_r))
 
     plt.scatter(target, relative, color='g', alpha=0.5)
 
@@ -221,7 +240,8 @@ def plotMeanXEnergy(target, predicted, lim_y=0.14, lim_l=0, lim_r=520, limit=Fal
     plt.figure(figsize=(5, 5))
     plt.xlabel("True energy (GeV)")
     plt.ylabel("Energy difference mean / Predicted energy")
-    plt.title("Mean, energies between %d and %d GeV" % (lim_l, lim_r))
+    plt.title("Mean. \n Energies between %d and %d GeV." % (lim_l, lim_r))
+    #plt.title("Mean")
 
     plt.scatter(target, np.mean(dif(target, predicted)) / predicted, color='g', alpha=0.5)
 
@@ -250,7 +270,8 @@ def plotStdXEnergy(target, predicted, lim_y=2.7, lim_l=0, lim_r=520, limit=False
     plt.figure(figsize=(5, 5))
     plt.xlabel("True energy (GeV)")
     plt.ylabel("Standard deviation of the energy difference / Predicted energy")
-    plt.title("Standard deviation, energies between %d and %d GeV" % (lim_l, lim_r))
+    plt.title("Standard deviation \n Energies between %d and %d GeV" % (lim_l, lim_r))
+    #plt.title("Standard deviation")
 
     plt.scatter(target, np.std(dif(target, predicted)) / predicted, color='g', alpha=0.5)
 
@@ -372,3 +393,85 @@ def plotRStds():
     plt.legend(loc='best')
     plt.show()
     # plt.savefig("std_rDif.jpg")
+    
+    
+def binning(nbins, label, pred):
+    if __package__ is None:
+        sys.path.append(os.path.realpath("/data/shared/Software/CMS_Deep_Learning"))
+    
+    from CMS_Deep_Learning.io import gen_from_data, retrieve_data
+    from CMS_Deep_Learning.postprocessing.metrics import distribute_to_bins
+    
+    out, x, y = distribute_to_bins(label, [label, pred], nb_bins=nbins, equalBins=True)
+    iSize = 500/nbins
+    
+    means = []
+    rMeans = []
+    stds = []
+    rStds = []
+    sizes = []
+    
+    for i in range(0, nbins):
+        sizes.append(len(x[i]))
+        plotPredictedXTarget(x[i], y[i], i*iSize, (i+1)*iSize)
+        histEDif(x[i], y[i], nbins=200, lim=20, lim_l=i*iSize, lim_r=(i+1)*iSize)
+        histRelDif(x[i], y[i], nbins=150, lim=0.15, lim_l=i*iSize, lim_r=(i+1)*iSize)
+        
+        difference = dif(x[i], y[i])
+        relDiff = rDif(x[i], y[i])
+        
+        mean = np.mean(difference)
+        means.append(mean)
+        
+        rMean = np.mean(relDiff)
+        rMeans.append(rMean)
+        
+        std = np.std(difference)
+        stds.append(std)
+        
+        rStd = np.std(relDiff)
+        rStds.append(rStd)
+        
+    
+    return x, y, means, rMeans, stds, rStds, sizes
+
+
+def plotN(means, stds, sizes, what):
+    plt.figure(figsize=(5, 5))
+    plt.xlabel("Energy bin")
+    
+    
+    if what == "means":
+        plt.ylabel("Mean of the energy difference (GeV)")
+        plt.title("Means")
+    
+    elif what == "stds":
+        plt.ylabel("Standard deviation of the energy difference (GeV)")
+        plt.title("Standard deviations")
+        
+    elif what == "rMeans":
+        plt.ylabel("Relative mean of the energy difference (GeV)")
+        plt.title("Relative means")
+    
+    elif what == "rStds":
+        plt.ylabel("Relative standard deviation of the energy difference (GeV)")
+        plt.title("Relative standard deviations")
+    
+    else:
+        print "The second parameter should be 'means', 'stds', 'rMeans' or 'rStds'. "
+    
+    n=len(means)
+    print n
+    iSize = 500/n
+    
+    for i in range(0, n):
+        error=stds[i]/np.sqrt(sizes[i])
+        #error = stds[i]
+        plt.scatter(i, means[i], color=tuple(np.random.random(3)), alpha=0.5, 
+                    label='%d to %d GeV' % (i*iSize, (i+1)*iSize))
+        plt.errorbar(i, means[i], yerr=error)
+        
+    plt.xlim(-0.9, 10)
+    plt.legend(loc='best', bbox_to_anchor=(1.52, 0.9))
+    plt.show()
+    # plt.savefig("means.jpg")
