3129bb1c7fc7cef38cad13e18668eed876efc6ef
vitoriapacela <vitoriapacela@gmail.com>
2017-08-02 13:19:50 +0200

    Naive sum implemented.

--- preprocessing.py
+++ preprocessing.py
@@ -14,3 +14,26 @@ def reshapeData(inp):
     (xe, xh), y = inp
     energy = [y[:, 1:]]
     return (xe, xh), energy
+
+
+def nSum(directory):
+    '''
+    Naive sum of the shower deposits in the HCAL and ECAL.
+    :param directory: path to the directory with HDF5 files.
+    :return: sum of the energies in the ECAL and HCAL, respectively.
+    '''
+    s_ecal = 0
+    s_hcal = 0
+
+    if (os.path.exists(os.path.abspath(directory)) and os.path.isdir(directory)):
+        directory = glob.glob(os.path.abspath(directory) + "/*.h5")
+
+    for fileName in directory:
+        inp = h5py.File(fileName, "r")
+        ecal = np.array(inp["ECAL"], dtype="float32")
+        hcal = np.array(inp["HCAL"], dtype="float32")
+
+        s_ecal += np.sum(ecal)
+        s_hcal += np.sum(hcal)
+
+    return s_ecal, s_hcal
