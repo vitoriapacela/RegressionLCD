cc385f236cd83fa355d9ec28d8d0fc16ea47f29f
Vitoria Barin Pacela <vitoriapacela@gmail.com>
2018-04-11 07:27:14 -0700

    setup fixed

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='RegressionLCD', version='0.1',
-    long_description=open(README.md).read(),
+    long_description=open('README.md').read(),
     url='https://github.com/vitoriapacela/RegressionLCD',
     author='Vitoria Barin Pacela',
     author_email='vitoriapacela@gmail.com',
-    packages=find_packages(exclude['tests']))
+    packages=find_packages()
+    )
