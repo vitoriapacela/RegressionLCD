1de69bb792058760957f12f1dcc18b9369508e94
Vitoria Barin Pacela <vitoriapacela@gmail.com>
2018-04-11 07:24:37 -0700

    long description added from README

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='RegressionLCD', version='0.1',
-        description='Deep Neural Networks for Imaging Calorimetry at LCD – SURF Caltech/CERN 2017.',
-        url='https://github.com/vitoriapacela/RegressionLCD',
-        author='Vitoria Barin Pacela',
-        author_email='vitoriapacela@gmail.com',
-        packages=find_packages(exclude['tests']))
+    long_description=open(README.md).read(),
+    url='https://github.com/vitoriapacela/RegressionLCD',
+    author='Vitoria Barin Pacela',
+    author_email='vitoriapacela@gmail.com',
+    packages=find_packages(exclude['tests']))
