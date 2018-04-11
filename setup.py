b27bcba6e5e28b57f4743fc8fe9ff44034ceb1e7
Vitoria Barin Pacela <vitoriapacela@gmail.com>
2018-04-11 07:18:17 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='RegressionLCD', version='0.1',
+        description='Deep Neural Networks for Imaging Calorimetry at LCD – SURF Caltech/CERN 2017.',
+        url='https://github.com/vitoriapacela/RegressionLCD',
+        author='Vitoria Barin Pacela',
+        author_email='vitoriapacela@gmail.com',
+        packages=find_packages(exclude['tests']))
